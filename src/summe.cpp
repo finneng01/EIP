9b9c9b36b480b94e7a5eab339066e2c3795b36c6
finneng01 <finn.engels01@gmail.com>
2025-04-23 11:45:43 +0200

    Summe Rekursiv

--- src/summe.cpp
+++ src/summe.cpp
@@ -1,22 +1,21 @@
 #include <cstdio>
 
-int summ(int x);
+int summe(int);
 
-int main () {
+int main() {
     int n;
-
     do {
         printf("Bitte n Eingeben");
         scanf("%d",&n);
     } while (n <= 0);
 
-    printf("%d = %d\n", n, summ(n));
-} //end main
+    printf("Die Summe von 1 bis %d ist %d\n", n, summe(n));
+}
 
-int summ (int x) {
+int summe(int x) {
     if (x == 1) {
-        return 1.0;
+        return 1;
     } else {
-        return (x+summ(x-1));
+        return x + summe(x - 1);
     }
 }
